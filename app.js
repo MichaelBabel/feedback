d3445bba1f6fe1ce36f40e4b4f4154a7caeeb538
Michael <68155390+MichaelBabel@users.noreply.github.com>
2023-03-19 20:11:41 -0300

    Adicionado popup após enviar a nota de avaliação

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ sixEl = document.getElementById("six"),
 sevenEl = document.getElementById("seven"),
 eightEl = document.getElementById("eight"),
 nineEl = document.getElementById("nine"),
-tenEl = document.getElementById("ten");
-let feedbackEl = document.getElementById("feedback");
-let btnEl = document.getElementById("send");
-let paragraphEl = document.getElementById("feedback-paragraph");
+tenEl = document.getElementById("ten"),
+feedbackEl = document.getElementById("feedback"),
+btnEl = document.getElementById("send"),
+paragraphEl = document.getElementById("feedback-paragraph"),
+popup = document.getElementById("popup"),
+container = document.getElementById("container"),
+closeEl = document.getElementById("close-popup");
 
 let arrayEl = [oneEl, twoEl, threeEl, fourEl, fiveEl, sixEl, sevenEl, eightEl, nineEl, tenEl];
 
@@ -19,5 +22,22 @@ for (let i = 0; i < arrayEl.length; i++) {
     feedbackEl.textContent = arrayEl[i].textContent;
     btnEl.disabled = false;
     paragraphEl.style.opacity = 1;
-});
+    })
+}
+
+btnEl.addEventListener("click", () => {
+    openPopup();
+})
+
+closeEl.addEventListener("click", () => {
+    closePopup();
+})
+
+function openPopup() {
+    popup.classList.add("open-popup");
+    container.style.opacity = 0;
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup");
 }
